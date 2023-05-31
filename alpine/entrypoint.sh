5b7c7ee7032d7200ec56fce85c3bc9a6340ed9ca
Sergey Starzhinskiy <36517638+sstarzh@users.noreply.github.com>
2023-05-31 12:20:07 -0400

    Change the port to 51820

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -3,5 +3,5 @@ arg1=${1}
 arg2=${2}
 arg3=${3}
 # Run the script in a connector mode. No systemd
-/usr/local/bin/ztedge-client new $arg1 $arg2 --connector --password $arg3 --listen-port 51220
+/usr/local/bin/ztedge-client new $arg1 $arg2 --connector --password $arg3 --listen-port 51820
 tail -f /dev/null
