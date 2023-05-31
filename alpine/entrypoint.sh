3b505925e4a14567d727cd4241b4334c729b026c
Sergey Starzhinskiy <36517638+sstarzh@users.noreply.github.com>
2023-05-31 09:25:13 -0400

    Upgraded to client v4.0.5 Now client can speficy listening port for return traffic (51220)

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -3,5 +3,5 @@ arg1=${1}
 arg2=${2}
 arg3=${3}
 # Run the script in a connector mode. No systemd
-/usr/local/bin/ztedge-client new $arg1 $arg2 --connector --password $arg3
+/usr/local/bin/ztedge-client new $arg1 $arg2 --connector --password $arg3 --listen-port 51220
 tail -f /dev/null
