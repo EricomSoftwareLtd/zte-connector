453d04b6014f0ebab2afc5b868605a5231e73b1b
Sergey Starzhinskiy <36517638+sstarzh@users.noreply.github.com>
2023-06-01 09:42:58 -0400

    Added options for connector mode and debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 arg1=${1}
 arg2=${2}
 arg3=${3}
-# Run the script in a connector mode. No systemd
-/usr/local/bin/ztedge-client new $arg1 $arg2 --connector --password $arg3 --debug
+arg4=${4}
+arg5=${5}
+# Run the script in a connector mode if arg4='--connector'. Enable debug if arg5='--debug'
+/usr/local/bin/ztedge-client new $arg1 $arg2 $arg4 --password $arg3 $arg5
 tail -f /dev/null
