f0169ddbf9f16d1fb3a038db0682cdfcb71c6959
Sergey Starzhinskiy <36517638+sstarzh@users.noreply.github.com>
2023-10-11 17:22:59 -0400

    Latest updates

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,17 @@
-#FROM node:lts-alpine3.9
-FROM node:alpine3.18
-COPY ./entrypoint.sh /
-RUN chmod +x /entrypoint.sh
+FROM ubuntu:22.04
+RUN apt-get update && apt-get -y upgrade
 RUN mkdir -p /opt/ericom
 COPY ztedge-client ztedge-client.js utils.js package.json package-lock.json ericomshield.crt ericomshield-new.crt /opt/ericom/
 RUN chmod +x /opt/ericom/ztedge-client
-RUN apk update && apk upgrade
-RUN apk --no-cache add ca-certificates wget
-RUN apk add --update dpkg wireguard-tools wireguard-go linux-headers at curl iproute2 iptables openssh openssl
-RUN apk add --update nodejs-current npm
+RUN apt-get update && apt-get install -y dpkg wireguard wireguard-go at curl libc-dev iproute2 iptables openssl net-tools vim
 RUN ln -s /opt/ericom/ztedge-client /usr/local/bin/ztedge-client
-RUN cd /opt/ericom && npm install --omit=dev
-COPY wg-quick /usr/bin/wg-quick
+RUN curl -sL https://deb.nodesource.com/setup_18.x | bash
+RUN apt-get install -y nodejs
+RUN cd /opt/ericom && npm install
+COPY wg-quick /usr/bin/wg-quick 
 RUN chmod +x /usr/bin/wg-quick
+COPY ./entrypoint.sh /
+RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
 CMD ["myTenant", "newConnector", "authKey", "apiKey", "tenantId"]
 EXPOSE 51821
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,47 +6,70 @@ arg4=${4}   #API key [OPTIONAL]
 arg5=${5}   #Tenant ID [OPTIONAL]
 
 LOG_FILE=/var/log/ztedge.log
-printf "$(date +%m-%d-%Y-%T) Container started \n" | tee ${LOG_FILE}
+
+# Start the script
+printf "$(date +%m-%d-%Y-%T) Container started \n" |& tee ${LOG_FILE}
+# Check external IP routine
+check_ext_ip () {
+    ext_ip=$(curl -s "https://ifconfig.me")
+    echo $ext_ip
+}
+check_ip () {
+    check_ext_ip
+    while [ -z $ext_ip];
+        do
+            printf "$(date +%m-%d-%Y-%T) External IP is NULL...Retrying in 10 seconds\n" |& tee -a ${LOG_FILE}
+            sleep 10
+            check_ext_ip
+        done
+    printf "$(date +%m-%d-%Y-%T) External IP is $ext_ip\n" |& tee -a ${LOG_FILE}
+    sleep 5
+}
 # Update the connector public IP if optional arguments are present.
 update_ip () {
-if [ -n "$arg4" ];
-then
-#build auth JSON
-printf "$(date +%m-%d-%Y-%T) Authenticating...\n" | tee -a ${LOG_FILE}
-auth='{"tenantID": "'"$arg5"'", "key": "'"$arg4"'" }'
-curl -s -c /tmp/cookies.txt -X POST -H "Content-Type: application/json" "https://ztadmin.ericomcloud.net/api/v1/auth" -d "$auth" -o /tmp/auth.json
-jwt=$(grep -o '"JWT".*' /tmp/auth.json| cut -d: -f2 |cut -d, -f1 | cut -d ' ' -f 2|tr -d '"' )
-cookie=$(grep -o 'route.*' /tmp/cookies.txt| awk '{$0=tolower($0);$1=$1}1' | cut -d ' ' -f 2 )
+    if [ -n "$arg4" ];
+        then
+            #build auth JSON
+            printf "$(date +%m-%d-%Y-%T) Authenticating...\n" |& tee -a ${LOG_FILE}
+            auth='{"tenantID": "'"$arg5"'", "key": "'"$arg4"'" }'
+            curl -s -c /tmp/cookies.txt -X POST -H "Content-Type: application/json" "https://ztadmin.ericomcloud.net/api/v1/auth" -d "$auth" -o /tmp/auth.json
+            jwt=$(grep -o '"JWT".*' /tmp/auth.json| cut -d: -f2 |cut -d, -f1 | cut -d ' ' -f 2|tr -d '"' )
+            cookie=$(grep -o 'route.*' /tmp/cookies.txt| awk '{$0=tolower($0);$1=$1}1' | cut -d ' ' -f 2 )
 
-#Update the connector public IP
-printf "$(date +%m-%d-%Y-%T) Determining the public IP...\n" | tee -a ${LOG_FILE}
-public_ip=$(curl -s "https://ifconfig.me")
-printf "$(date +%m-%d-%Y-%T) Public IP: $public_ip\n" | tee -a ${LOG_FILE}
-printf "$(date +%m-%d-%Y-%T) Updating the connector public IP on ZTEdge side...\n" | tee -a ${LOG_FILE}
-upd_json='{"public_ip": "'"$public_ip"'" }'
-curl -s -X PATCH "https://ztadmin.ericomcloud.net/api/v1/ztna/connector/$arg2" -H "Content-Type: application/json" -H "Authorization: Bearer "$jwt"" -H "Cookie: route=$cookie" -d "$upd_json" --write-out '%{http_code}'
-printf "$(date +%m-%d-%Y-%T) Done\n" | tee -a ${LOG_FILE}
-sleep 5
-fi
+            #Update the connector public IP
+            printf "$(date +%m-%d-%Y-%T) Determining the public IP...\n" |& tee -a ${LOG_FILE}
+            check_ip
+            public_ip=$ext_ip
+            printf "$(date +%m-%d-%Y-%T) Updating the connector public IP on ZTEdge side...\n" |& tee -a ${LOG_FILE}
+            upd_json='{"public_ip": "'"$public_ip"'" }'
+            curl -s -X PATCH "https://ztadmin.ericomcloud.net/api/v1/ztna/connector/$arg2" -H "Content-Type: application/json" -H "Authorization: Bearer "$jwt"" -H "Cookie: route=$cookie" -d "$upd_json" --write-out '%{http_code}'
+            printf "$(date +%m-%d-%Y-%T) Done\n" |& tee -a ${LOG_FILE}
+            sleep 5
+    fi
 }
 
 # Run update_ip func
 update_ip
 # Run the script in a connector mode.
-printf "$(date +%m-%d-%Y-%T) Starting the connector...\n" | tee -a ${LOG_FILE}
+printf "$(date +%m-%d-%Y-%T) Starting the connector...\n" |& tee -a ${LOG_FILE}
 /usr/local/bin/ztedge-client new $arg1 $arg2 --connector --password $arg3 --listen-port 51820 --health-check-port 51821 &
 while true;
-do
-sleep 60;
-check_ip=$(curl -s "https://ifconfig.me")
-    if [ $check_ip == $public_ip ];
-    then
-        printf "."
-    else
-        printf "\n $(date +%m-%d-%Y-%T) Connector public IP changed: $check_ip. Updating IP in ZTEdge now...\n" | tee -a ${LOG_FILE}
-        update_ip
-        /usr/local/bin/ztedge-client down
-        sleep 5
-        /usr/local/bin/ztedge-client up &
-    fi
-done
+    do
+    sleep 60;
+    check_ip=$(check_ext_ip)
+        if [ $check_ip == $public_ip ];
+        then
+            printf "."
+        else
+            if [ -z $check_ip ];
+                then
+                    printf "$(date +%m-%d-%Y-%T) Unable to determine external IP!\n" |& tee -a ${LOG_FILE}
+                else
+                    printf "$(date +%m-%d-%Y-%T) Connector public IP changed: $check_ip. Updating IP in ZTEdge now...\n" |& tee -a ${LOG_FILE}
+            fi
+            update_ip
+            /usr/local/bin/ztedge-client down
+            sleep 5
+            /usr/local/bin/ztedge-client up &
+        fi
+    done
